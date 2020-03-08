8d8e7a7de57292cc269dae2bdb30f293a9c3547f
Adelino Masioli <alfjuniorbh.web@gmail.com>
2020-03-08 20:21:51 +0000

    Fix model like user simbol

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,5 +2,5 @@ class Like < ApplicationRecord
   belongs_to :post
   belongs_to :user
 
-  validates :post_id, uniqueness: { scope: user_id, message: "Post alredy liked" }
+  validates :post_id, uniqueness: { scope: :user_id, message: "Post alredy liked" }
 end

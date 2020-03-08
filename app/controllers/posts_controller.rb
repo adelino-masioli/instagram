d38a2398d48a58f205f35d863951213b8c3f8746
Adelino Masioli <alfjuniorbh.web@gmail.com>
2020-03-08 19:58:22 +0000

    Websockets

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,6 +19,7 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(post_params.merge(created_by: current_user))
       if @post.save
+        PostChannel.broadcast_to "post_channel", post_created: render_to_string(partial: @post)
         redirect_to @post, notice: 'Post was successfully created.'
       else
         flash.now[:alert] = @post.errors.full_messages.to_sentence
